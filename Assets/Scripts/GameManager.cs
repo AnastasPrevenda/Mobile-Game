93afda00d3b31f47c75042d2ad22164461cde73a
AnastasPrevenda <stu2101321089@uni-plovdiv.bg>
2025-01-15 11:56:20 +0200

    Random Block Drop

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 public class NewEmptyCSharpScript : MonoBehaviour
 {
 
-    void Start()
-{
+    public GameObject block;
+    public float maxX;
+    public Transform spawnPoint;
 
-}
+    public float spawnRate;
+bool gameStarted = false;
+
 
 void Update()
 {
-
+   if(Input.GetMouseButtonDown(0) && !gameStarted)
+   {
+       StartSpawning();
+       gameStarted = true;
+   }
 }
 
-    
+
+void StartSpawning(){
+    InvokeRepeating("SpawnBlock", 0.5f, spawnRate);
+}
+
+
+private void SpawnBlock()
+{
+    Vector3 spawnPos = spawnPoint.position;
+    spawnPos.x = Random.Range(-maxX, maxX);
+
+    Instantiate(block, spawnPos, Quaternion.identity);
+
+}
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+
+    public float speed;
+    Rigidbody2D rb;
     void Start()
 {
-
+   rb = GetComponent<Rigidbody2D>();
 }
 
 void Update()
 {
-
+    if(Input.GetMouseButton(0))
+    {
+        Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+ if(touchPos.x < 0){
+        rb.AddForce(Vector2.left * speed);
+ }else{
+        rb.AddForce(Vector2.right * speed);
+ }
+        
+    }else{
+        rb.linearVelocity = Vector2.zero;
+    }
 }
 
+
+
+void OnCollisionEnter2D(Collision2D col)
+{
+    if(col.gameObject.tag == "Block")
+    {
+        SceneManager.LoadScene(0);
+    }
+}
 
 
 }
